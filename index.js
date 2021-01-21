65705ea734f0cb02261e0469723c97a4b46f7769
Zack Jameson <zacharymjameson@gmail.com>
2021-01-20 20:49:02 -0500

    html and js progress

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 'use strict'
 
 const apiKey = 'O1hykHcgTDB8Tv12NAWQ0G6DgAwQj88u2Ctgi0Wb';
-const searchURL =  `https://developer.nps.gov/api/v1/parks?parkCode=hoba&stateCode=NC`;
+const searchURL =  `https://developer.nps.gov/api/v1/parks`;
 
-
+function getParks(parkQuery, stateQuery, maxResults=10) {
+    const params = {
+        key: apiKey,
+        parkCode: parkQuery,
+        stateCode: stateQuery,
+        maxResults,
+    }
+}
 
 function watchForm () {
     $('form').submit(event => {
         event.preventDefault();
-        const parkSearch = $('#js-park-search').val();
+        const parkSearch = $('#js-parkcode').val();
+        const stateSearch = $('#js-statecode').val();
         const maxResults = $('#js-max-results').val();
-        getParks(parkSearch, maxResults);
+        getParks(parkSearch, stateSearch, maxResults);
     });
 }
 
